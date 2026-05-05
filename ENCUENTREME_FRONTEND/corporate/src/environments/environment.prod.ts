a6af5a01ff45b26fafbfb968d73154f88468ff5a
Miguel Licla <liclainciso0@gmail.com>
2026-05-04 21:17:05 -0500

    fix(frontend): clean placeholders and use prebuild hook for env injection

--- ENCUENTREME_FRONTEND/corporate/src/environments/environment.prod.ts
+++ ENCUENTREME_FRONTEND/corporate/src/environments/environment.prod.ts
@@ -1,12 +1,12 @@
 export const environment = {
   production: true,
   defaultauth: 'api',
-  apiUrl: 'YOUR_PRODUCTION_API_URL', // Reemplazar con URL de producción
-  adsenseClientId: 'ca-pub-2715739910930216',
+  apiUrl: '',
+  adsenseClientId: '',
   cloudinary: {
-    cloudName: 'YOUR_CLOUDINARY_NAME',
-    apiKey: 'YOUR_CLOUDINARY_API_KEY',
-    apiSecret: 'YOUR_CLOUDINARY_API_SECRET',
+    cloudName: '',
+    apiKey: '',
+    apiSecret: '',
     folder: 'encuentreme'
   },
   firebaseConfig: {
--- ENCUENTREME_FRONTEND/corporate/src/environments/environment.ts
+++ ENCUENTREME_FRONTEND/corporate/src/environments/environment.ts
@@ -1,16 +1,12 @@
-// This file can be replaced during build by using the `fileReplacements` array.
-// `ng build` replaces `environment.ts` with `environment.prod.ts`.
-// The list of file replacements can be found in `angular.json`.
-
 export const environment = {
   production: false,
   defaultauth: 'api',
-  apiUrl: 'http://localhost:5001',
-  adsenseClientId: 'ca-pub-2715739910930216',
+  apiUrl: '',
+  adsenseClientId: '',
   cloudinary: {
-    cloudName: 'YOUR_CLOUDINARY_NAME',
-    apiKey: 'YOUR_CLOUDINARY_API_KEY',
-    apiSecret: 'YOUR_CLOUDINARY_API_SECRET',
+    cloudName: '',
+    apiKey: '',
+    apiSecret: '',
     folder: 'encuentreme'
   },
   firebaseConfig: {
@@ -24,12 +20,3 @@ export const environment = {
     measurementId: ''
   }
 };
-
-/*
- * For easier debugging in development mode, you can import the following file
- * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
- *
- * This import should be commented out in production mode because it will have a negative impact
- * on performance if an error is thrown.
- */
-// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
